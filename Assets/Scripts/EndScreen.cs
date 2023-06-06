74771364c9c272287cbf9caec67145529e7aff1e
Peterson Guo <petersonguo@gmail.com>
2023-06-05 22:32:12 -0400

    End Screen

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class EndScreen : MonoBehaviour
 {
+    public Text scoreText;
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+        scoreText.text = score.ToString() + " POINTS";
+    }
+
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu");
